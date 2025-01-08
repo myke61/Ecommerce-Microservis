616b84f081a5499da56b75d582827bda32434f67
Agirman <mikail.agirman@adesso.com.tr>
2025-01-08 11:47:06 +0300

    Initial Commit With Ocelot

---
+++ Ecommerce-Microservis/ApiGateway/Program.cs
@@ -0,0 +1,27 @@
+using Ocelot.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot();
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
