8145318735beaaa2a373e1433da6d00e77468049
myke61 <mikailagirman61@gmail.com>
2025-03-27 14:07:20 +0300

    ProductServiceAdded

--- Ecommerce-Microservis/ApiGateway/obj/Debug/net9.0/ApiGateway.AssemblyInfo.cs
+++ Ecommerce-Microservis/ApiGateway/obj/Debug/net9.0/ApiGateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+616b84f081a5499da56b75d582827bda32434f67")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
