96b6a60e4c32e097d19d86c2178f217ce8922f14
myke61 <mikailagirman61@gmail.com>
2025-04-19 01:22:16 +0300

    ocelot.json updated and endpoints work with it

--- Ecommerce-Microservis/ApiGateway/Program.cs
+++ Ecommerce-Microservis/ApiGateway/Program.cs
@@ -1,4 +1,5 @@
 using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,7 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+app.UseOcelot().Wait();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
